4c14e2519689b2e7325619e32c5aa93e881cda94
MorseWayne <morsewayne98@gmail.com>
2026-09-25 14:11:01 +0800

    docs(course): add long-term IM engineering responsibility chapter

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -382,6 +382,7 @@ export default sidebar({
             "curriculum/13_architecture/09_cross_team_delivery",
             "curriculum/13_architecture/10_technical_debt_evolution",
             "curriculum/13_architecture/11_complex_im_defense",
+            "curriculum/13_architecture/12_engineer_long_term_responsibility",
             "curriculum/13_architecture/core"
           ]
         },
