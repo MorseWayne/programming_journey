0d7e84c1e5299b95233b7b468b4453ce28e49001
MorseWayne <morsewayne98@gmail.com>
2026-09-25 10:16:53 +0800

    docs(course): add IM load testing and capacity chapter

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -335,6 +335,7 @@ export default sidebar({
             "curriculum/11_reliability/04_diagnostic_method",
             "curriculum/11_reliability/05_cpu_memory_performance",
             "curriculum/11_reliability/06_io_network_performance",
+            "curriculum/11_reliability/07_load_testing_capacity",
             "curriculum/11_reliability/core"
           ]
         },
